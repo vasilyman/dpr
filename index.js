5f87b4ad35846a573d8c91a2bee64e137c5cad39
rinvas <vasilyman@mail.ru>
2023-10-24 22:24:01 +0500

    wip: fix performance

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import predlogs from "./predlogi.json";
 
 const regexFn = new RegExp(
-  `(^|\\p{P}|\\p{S}|\\p{Z}|\\p{C}|\\p{M}|\\p{N})(${predlogs.join("|")})([\\r\\n\\t\\f\\v \\u1680\\u2000-\\u200a\\u2028\\u2029\\u202f\\u205f\\u3000\\ufeff])`,
+  `(^|\\p{P}|\\p{S}|\\p{Z}|\\p{C}|\\p{M}|\\p{N})(${predlogs.join(
+    "|"
+  )})([\\r\\n\\t\\f\\v \\u1680\\u2000-\\u200a\\u2028\\u2029\\u202f\\u205f\\u3000\\ufeff])`,
   "gmiu"
 );
 
 const addNbspBeforeRegexp = (el, rFn) => {
+
   el.childNodes.forEach(node => {
     if (node.nodeType === Node.TEXT_NODE) {
       let temp = node.textContent;
@@ -15,8 +18,16 @@ const addNbspBeforeRegexp = (el, rFn) => {
       node.textContent = temp;
     } else {
       setTimeout(() => {
-        if (process.env.NODE_ENV === 'development') performance.mark('predlogi tick')
+        let start;
+        if (process.env.NODE_ENV === "development") {
+          start = performance.now();
+          performance.mark(`predlogi start tick`);
+        }
         addNbspBeforeRegexp(node, rFn);
+        performance.mark(
+          `predlogin end (after upd Vue): ${performance.now() -
+          start}`
+        );
       }, 0);
     }
   });
@@ -24,19 +35,17 @@ const addNbspBeforeRegexp = (el, rFn) => {
 
 /**
  * Плагин добавляет директиву v-predlogi
- * проходится по элементу и всем его потомках добавляя перед всеми предлогами `&nbsp;`
- * чтобы избавиться от висячих предлогов
+ * проходится по элементу и всем его потомках добавляя перед всеми предлогами `&nbsp;` (непереносимый пробел)
+ * **чтобы избавиться от висячих предлогов**
  */
 export default {
   install(Vue) {
     Vue.directive("predlogi", {
       bind(el) {
         addNbspBeforeRegexp(el, regexFn);
-        if (process.env.NODE_ENV === 'development') performance.mark('predlogin end');
       },
       updated(el) {
         addNbspBeforeRegexp(el, regexFn);
-        if (process.env.NODE_ENV === 'development') performance.mark('predlogin end (after update vue)');
       }
     });
   }
